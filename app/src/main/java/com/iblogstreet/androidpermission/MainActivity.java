82d56536fdfa2f498c820421c3ea2b12e1978262
wangjun <wang.jun16@163.com>
2018-11-23 17:29:41 +0800

    修改兼容包

--- app/src/main/java/com/iblogstreet/androidpermission/MainActivity.java
+++ app/src/main/java/com/iblogstreet/androidpermission/MainActivity.java
@@ -3,7 +3,6 @@ package com.iblogstreet.androidpermission;
 import android.app.Activity;
 import android.os.Bundle;
 
-import com.iblogstreet.permissionlib.permission.PermissionUtil;
 
 public class MainActivity extends Activity {
 
