e4c4d1d9f3c1077b19da2d4a4450c3ee8cd00ee1
wangjun <wang.jun16@163.com>
2018-11-23 17:10:51 +0800

    修改MainActivity”

--- app/src/main/java/com/iblogstreet/androidpermission/MainActivity.java
+++ app/src/main/java/com/iblogstreet/androidpermission/MainActivity.java
@@ -1,9 +1,11 @@
 package com.iblogstreet.androidpermission;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.iblogstreet.permissionlib.permission.PermissionUtil;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
